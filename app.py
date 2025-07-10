da5a30810c3eb16df8a2397b4f9d982a16045b1e
prozect33 <prozect33@gmail.com>
2025-07-10 14:41:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ with tab1:
                     ])
                 ]:
                     st.markdown(f"""
-<div style='display: grid; grid-template-columns: 0.1fr 1fr 1fr 0.3fr 0.3fr; background: {bg_color}; padding: 12px 18px; border-radius: 10px; text-align: center; align-items: center; gap: 8px; margin-bottom: 12px;'>
+<div style='display: grid; grid-template-columns: 1fr 1fr 1fr 0.3fr 0.3fr; background: {bg_color}; padding: 12px 18px; border-radius: 10px; text-align: center; align-items: center; gap: 8px; margin-bottom: 12px;'>
     <div><div style='font-weight:bold; font-size:15px;'>{values[0][0]}</div><div style='font-size:15px;'>{values[0][1]}</div></div>
     <div><div style='font-weight:bold; font-size:15px;'>{values[1][0]}</div><div style='font-size:15px;'>{values[1][1]}</div></div>
     <div><div style='font-weight:bold; font-size:15px;'>{values[2][0]}</div><div style='font-size:15px;'>{values[2][1]}</div></div>
