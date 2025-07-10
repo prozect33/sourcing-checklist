c5d67f04518f4a909d5fbcf799ab8149ac31951d
prozect33 <prozect33@gmail.com>
2025-07-10 10:42:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ with tab1:
                         st.markdown(f"**{row_labels[i]}**")
                         st.markdown(f"<div style='font-size: 16px;'>{row_values[i]}</div>", unsafe_allow_html=True)
 
-                st.markdown("<div style='height: 80px;'></div>", unsafe_allow_html=True)
+                st.markdown("<div style='height: 85px;'></div>", unsafe_allow_html=True)
 
                 with st.expander("📦 상세 비용 항목 보기", expanded=False):
                     st.markdown(f"**판매가:** {format_number(sell_price)}원")
