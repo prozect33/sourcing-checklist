6d7565a246a4ba463c06d484a88f5dafd296f909
prozect33 <prozect33@gmail.com>
2025-07-12 23:25:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -195,6 +195,8 @@ def main():
                 st.markdown(f"- 📉 최소마진율: {(profit2/supply_price2*100):.2f}%")
                 st.markdown(f"- 💹 ROI: {roi:.2f}% / 마진 기준 ROI: {roi_margin:.2f}%")
                 
+                st.markdown("<div style='height:20px;'></div>", unsafe_allow_html=True)
+                
                 with st.expander("📦 상세 비용 항목 보기", expanded=False):
                     def styled_line(label, value):
                         return f"<div style='font-size:15px;'><strong>{label}</strong> {value}</div>"
