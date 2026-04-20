807a6efa682b33efc72cd75ce20c1922825315fc
prozect33 <prozect33@gmail.com>
2026-04-20 16:58:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1471,7 +1471,6 @@ def main():
                             st.session_state["sold_product_filter_global"] = "전체"
 
                         selected_product = st.selectbox(
-                        selected_product = st.selectbox(
                             "📦 상품 선택",
                             product_options,
                             key=f"sold_product_filter_{i}",
